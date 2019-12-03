6c87b6769f76e088c33f5556da3e1b7337fec1ce
ericdude4 <eric_froese2@hotmail.com>
2019-12-03 10:54:06 -0500

    update and delete working (only test for time_entries)

--- lib/resource.ex
+++ lib/resource.ex
@@ -66,7 +66,7 @@ defmodule FreshBex.Resource do
 
     inject =
       case Keyword.get(opts, :only) do
-        nil -> [:get, :list, :create]
+        nil -> [:get, :list, :create, :update, :delete]
         list -> list
       end
 
@@ -260,7 +260,7 @@ defmodule FreshBex.Resource do
                        FreshBex.get_client(Keyword.get(options, :access_token))
                      ) do
                   {:ok, %OAuth2.Client{} = client} ->
-                    get(options ++ [access_token: client.token])
+                    create(properties, options ++ [access_token: client.token])
 
                   {:error, %OAuth2.Response{status_code: 401, body: body}} ->
                     raise(FreshBexError, "Invalid refresh token")
@@ -276,6 +276,138 @@ defmodule FreshBex.Resource do
         end
       end
 
-    [api_url, pagination_data, resource_path, nested_resource, get, list, create]
+    update =
+      if :update in inject do
+        quote do
+          @doc """
+          Update given #{__MODULE__} resource. Parameters are not validated, but passed along in the request body. Please see FreshBooks API documentation for the appropriate body parameters for your resource.
+
+          ## Params
+          * `resource` - `#{__MODULE__}` being updated.
+          * `changes` - Map of changes to be applied to the resource.
+          """
+          def update(resource, changes, options \\ []) do
+            url = "#{api_url(options)}/#{resource_path()}/#{resource.id}"
+
+            resource_name = resource() |> String.replace("ies", "y")
+
+            body =
+              case Jason.encode(Map.put(%{}, resource_name, changes)) do
+                {:ok, body} ->
+                  body
+
+                {:error, %Jason.EncodeError{message: message}} ->
+                  raise(
+                    FreshBexError,
+                    "Invalid :changes. Was not able to encode to JSON. #{message}"
+                  )
+              end
+
+            case OAuth2.Client.put(
+                   FreshBex.get_client(Keyword.get(options, :access_token)),
+                   url,
+                   body,
+                   [{"Content-Type", "application/json"}]
+                 ) do
+              {:ok, %OAuth2.Response{body: resource}} ->
+                resource =
+                  Jason.decode!(resource, keys: :atoms)
+                  |> (fn map ->
+                        # singluarize the resource() method so we know where to find the data
+                        key =
+                          if String.ends_with?(resource(), "ies") do
+                            String.replace_suffix(resource(), "ies", "y")
+                          else
+                            String.replace_suffix(resource(), "s", "")
+                          end
+                          |> String.to_atom()
+
+                        map[key]
+                      end).()
+
+                struct!(__MODULE__, resource)
+
+              {:error, %OAuth2.Response{status_code: 422, body: body}} ->
+                {:error, Jason.decode!(body)}
+
+              {:error, %OAuth2.Response{status_code: 401, body: body}} ->
+                # run refresh then retry
+                case OAuth2.Client.refresh_token(
+                       FreshBex.get_client(Keyword.get(options, :access_token))
+                     ) do
+                  {:ok, %OAuth2.Client{} = client} ->
+                    update(resource, changes, options ++ [access_token: client.token])
+
+                  {:error, %OAuth2.Response{status_code: 401, body: body}} ->
+                    raise(FreshBexError, "Invalid refresh token")
+
+                  {:error, %OAuth2.Error{reason: reason}} ->
+                    raise(FreshBexError, "Error: #{inspect(reason)}")
+                end
+
+              {:error, %OAuth2.Error{reason: reason}} ->
+                raise(FreshBexError, "Error: #{inspect(reason)}")
+            end
+          end
+        end
+      end
+
+    delete =
+      if :delete in inject do
+        quote do
+          @doc """
+          Delete FreshBooks #{__MODULE__} with provided id.
+
+          Will return `:ok` on success, `{:error, 404}` if resource not found, or throw `HarvexError` for implementation errors.
+
+          ## Params
+          * `id` - id of #{__MODULE__} resource to delete
+          """
+          def delete(id, options \\ []) do
+            url = "#{api_url(options)}/#{resource_path()}/#{id}"
+
+            case OAuth2.Client.delete(
+                   FreshBex.get_client(Keyword.get(options, :access_token)),
+                   url
+                 ) do
+              {:ok, resp} ->
+                case resp.status_code do
+                  204 ->
+                    :ok
+
+                  404 ->
+                    {:error, 404}
+
+                  status_code when status_code > 399 ->
+                    error = Jason.decode!(resp.body, keys: :atoms)
+                    raise(FreshBexError, error)
+                end
+
+              {:error, %OAuth2.Response{status_code: 422, body: body}} ->
+                {:error, Jason.decode!(body)}
+
+              {:error, %OAuth2.Response{status_code: 401, body: body}} ->
+                # run refresh then retry
+                case OAuth2.Client.refresh_token(
+                       FreshBex.get_client(Keyword.get(options, :access_token))
+                     ) do
+                  {:ok, %OAuth2.Client{} = client} ->
+                    delete(id, options ++ [access_token: client.token])
+
+                  {:error, %OAuth2.Response{status_code: 401, body: body}} ->
+                    raise(FreshBexError, "Invalid refresh token")
+
+                  {:error, %OAuth2.Error{reason: reason}} ->
+                    raise(FreshBexError, "Error: #{inspect(reason)}")
+                end
+
+              {:error, %OAuth2.Error{reason: reason}} ->
+                raise(FreshBexError, "Error: #{inspect(reason)}")
+            end
+          end
+        end
+      end
+
+    [api_url, pagination_data, resource_path, nested_resource, get, list, create, update, delete]
   end
 end
--- test/fresh_bex_test.exs
+++ test/fresh_bex_test.exs
@@ -57,4 +57,49 @@ defmodule FreshBexTest do
       assert %FreshBex.TimeEntry{} = FreshBex.TimeEntry.create(properties)
     end
   end
+
+  test "update time entry" do
+    use_cassette "time_entry.update" do
+      time_entry = %FreshBex.TimeEntry{
+        active: true,
+        billable: true,
+        billed: false,
+        client_id: 201_781,
+        created_at: "2019-12-02T22:23:36Z",
+        duration: 4000,
+        duration_rounded_to_nearest_minute: 4000,
+        id: 60_626_711,
+        identity_id: 2_804_980,
+        internal: false,
+        is_logged: true,
+        note: "test",
+        pending_client: nil,
+        pending_project: nil,
+        pending_task: nil,
+        project_id: 3_624_248,
+        retainer_id: nil,
+        service_id: nil,
+        started_at: "2019-12-01T20:00:00Z",
+        task_id: nil,
+        timer: nil
+      }
+
+      changes = %{
+        is_logged: true,
+        duration: 5000,
+        note: "test",
+        started_at: "2019-12-01T20:00:00.000Z",
+        client_id: 201_781,
+        project_id: 3_624_248
+      }
+
+      assert %FreshBex.TimeEntry{duration: 5000} = FreshBex.TimeEntry.update(time_entry, changes)
+    end
+  end
+
+  test "delete time entry" do
+    use_cassette "time_entry.delete" do
+      assert :ok = FreshBex.TimeEntry.delete(60_626_711)
+    end
+  end
 end
